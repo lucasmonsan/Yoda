33b12fc154b29161230cd8eff69fc4dc41acc9ac
lucasmonsan <lucasmonsan@gmail.com>
2023-01-18 00:00:47 -0300

    Estrutura inicial do Header criada

--- src/styles/Texts.jsx
+++ src/styles/Texts.jsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 export const Title = styled.strong`
   font-size: ${props => props.size};
   font-weight: ${props => props.weight};
+  color: ${props => props.color};
 `
 export const Text = styled.span`
   font-size: ${props => props.size};
   font-weight: ${props => props.weight};
+  color: ${props => props.color};
 `
